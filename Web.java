43dc36efb8443a05a6724087b764189ef4627ece
nahsc0 <naseeryusuf07@gmail.com>
2024-02-19 17:48:38 +0100

    my java web app

---
+++ Web.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class Nasco {
+    public static void main(String[] args) {
+        int age = 27;
+        age = 45;
+        
+        //Generating a Primitive Type Data
